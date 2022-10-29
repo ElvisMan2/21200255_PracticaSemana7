f841c76b75da27467764898a1a4ab116a665d68f
Elvis <elvis.manco.m@gmail.com>
2022-10-29 14:58:12 -0500

    primer commit

---
+++ src/main/java/modelo/Vuelo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author Elvis
+ */
+public class Vuelo {
+    private String codigo;
+    private String fecha;
+    private String hora;
+}
